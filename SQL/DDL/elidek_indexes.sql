2107ce3dcf9d872fa287399116550fe5ed530b26
hackLinos <62839948+hackLinos@users.noreply.github.com>
2022-06-05 21:01:01 +0300

    Add files via upload

---
+++ SQL/DDL/elidek_indexes.sql
@@ -0,0 +1,5 @@
+CREATE INDEX idx_ID_ereuniti ON ereunitis (ID_ereuniti) 
+CREATE INDEX idx_ID_ergou ON ergo (ID_ergou) 
+CREATE INDEX idx_ID_stelehous ON stelehos (ID_stelehous)   
+CREATE INDEX idx_ID_epistimoniko_pediou ON epistimoniko_pedio (ID_epistimoniko_pediou) 
+
